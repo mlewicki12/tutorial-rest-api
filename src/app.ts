415a33d343409b53db20cd9929ab592cfd59a7a5
Maciej Lewicki <lewickimapl@gmail.com>
2022-02-21 17:41:19 +0100

    add db connect + utils

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 
 import express from 'express';
+import config from 'config';
+
+import connect from './utils/connect';
+import logger from './utils/logger';
 
 const app = express();
 
-app.listen(1337, () => {
-  console.log('app is running');
+const port = config.get<number>('port');
+app.listen(port, async () => {
+  await connect();
+  logger.info(`app is running at http://localhost:${port}`);
 });
